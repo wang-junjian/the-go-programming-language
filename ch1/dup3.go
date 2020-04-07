d4c3db2b34cc5795aee34115665a948d988a0d13
wang-junjian <wang-junjian@qq.com>
2020-04-07 21:20:50 +0800

    Create dup3.go

---
+++ ch1/dup3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	for _, file := range files {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			continue
+		}
+
+		lines := strings.Split(string(data), "\n")
+		for _, line := range lines {
+			counts[line]++
+		}
+	}
+
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
+}
