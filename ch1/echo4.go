f8df462994c427795d2cdf8a1531d701c5bc3e36
wang-junjian <wang-junjian@qq.com>
2020-04-08 17:14:51 +0800

    Update echo4.go

--- ch1/echo4.go
+++ ch1/echo4.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"flag"
+	"strings"
 )
 
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
 func main() {
-	fmt.Println(os.Args[1:])
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
 }
